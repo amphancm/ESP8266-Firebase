67a71ff51872f94ec7bcb42ee78f8cfff7ab69e0
Adun Nantakaew <amphancm@gmail.com>
2016-12-28 13:18:11 +0700

    Add files via upload

---
+++ declare.h
@@ -0,0 +1,68 @@
+
+#define DHTPIN 10               // what pin we're connected to
+
+//#define DHTTYPE DHT11         // DHT 11 
+#define DHTTYPE DHT22           // DHT 22  (AM2302)
+//#define DHTTYPE DHT21         // DHT 21  (AM2301)
+
+DHT dht(DHTPIN, DHTTYPE,15);
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_WIDTH  16 
+//static const unsigned char PROGMEM logo16_glcd_bmp[] =
+static const unsigned char logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+int ledState = LOW;  
+int showState = LOW; 
+int lp = 0;
+int lp_time = 30;            // 2000 mS x lp_time = Sent data time
+int imode = 0;
+int buttonState = 0;         // variable for reading the pushbutton status
+
+boolean cloop = true;
+
+unsigned long previousMillis = 0;
+const long interval = 2000;
+
+
+// WIFI
+#define WIFI_SSID        "YOUR SSID"  
+#define WIFI_PASSPHARSE  "YOUR PASSWORD"
+
+
+// Config Firebase
+#define FIREBASE_HOST   "YOUR FIREBASE HOST"
+#define FIREBASE_AUTH   "YOUR AUTH KEY"
+
+
+
+
+
---
+++ esp8266_firebase.ino
@@ -0,0 +1,307 @@
+#include <ESP8266WiFi.h>
+#include <WiFiConnector.h>
+#include <FirebaseArduino.h>
+
+WiFiConnector *wifi;
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <Wire.h>
+#include <DHT.h>
+#include <Adafruit_GFX.h>
+#include <ESP_Adafruit_SSD1306.h>
+
+#include "declare.h"
+#include "init_wifi.h"
+
+
+void init_hardware(){
+  Serial.begin(115200);
+  delay(10);
+  Serial.println();
+  Serial.println("BEGIN");
+}
+
+void showTemp(float temp,float hud) {
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("   Temp  ");
+  
+  display.setTextSize(3);                                 // Size4 = 5 digit , size3 = 7 digits
+  //display.setTextColor(BLACK, WHITE);                   // 'inverted' text
+  display.setTextColor(WHITE);
+  display.setCursor(0,24);
+ 
+  display.print(temp);
+  display.println("C"); 
+  display.display();
+}
+
+void showHud(float temp,float hud) {
+
+  display.clearDisplay();  
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println(" Humidity ");
+   
+  display.setTextSize(3);                               // Size4 = 5 digit , size3 = 7 digits
+  //display.setTextColor(BLACK, WHITE);                 // 'inverted' text
+  display.setTextColor(WHITE);
+  display.setCursor(0,24);
+ 
+  display.print(hud);
+  display.println("%");  
+  display.display();
+}
+
+
+//==================================================
+void setup()   {                
+//==================================================
+  
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, HIGH);
+  
+  dht.begin();            // dht begin
+
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  //display.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // initialize with the I2C addr 0x3D (for the 128x64)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x78>>1);
+  // init done
+  
+
+  // Clear the buffer.
+  display.clearDisplay();
+    
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  //display.setTextColor(BLACK, WHITE); // 'inverted' text
+  display.setCursor(0,0);
+  display.println("  Weather  ");
+  
+  display.setTextSize(3);               // Size4 = 5 digit , size3 = 7 digits
+  //display.setTextColor(BLACK, WHITE); // 'inverted' text
+  display.setTextColor(WHITE);
+  display.setCursor(0,18);
+  display.println("Station");
+    
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  //display.setTextColor(BLACK, WHITE); // 'inverted' text
+  display.setCursor(0,52);
+  display.println("Version 0.4");
+ 
+  display.display();
+  delay(2000);
+  
+  
+  // WiFi Connect
+  Serial.print("CONNECTING TO ");
+  Serial.println(wifi->SSID() + ", " + wifi->psk());
+  
+  display.clearDisplay();  
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  //display.setTextColor(BLACK, WHITE); // 'inverted' text
+  display.setCursor(0,0);
+  display.println("Connecting");
+   
+
+  wifi->on_connecting([&](const void* message){
+    char buffer[70];
+    //sprintf(buffer, "[%d] connecting -> %s ", wifi->counter, (char*) message);
+    //Serial.println(buffer);
+        
+    delay(500);
+    Serial.print(".");
+    display.print(".");
+    display.display();
+    //lp++;
+    //if(lp>=20) break;
+  });
+
+  
+  
+  wifi->on_smartconfig_waiting([&](const void* message){
+    display.clearDisplay();    
+    display.setTextSize(2);       display.setTextColor(WHITE);  
+    display.setCursor(0,0);       display.println("Smart");  
+    display.setCursor(0,18);      display.println("CONFIG");
+    //display.setCursor(0,36);      display.println(WiFi.localIP()); 
+    display.display();      
+  });
+  
+  
+
+  wifi->on_connected([&](const void* message){
+    // Print the IP address
+    Serial.print("WIFI CONNECTED ");
+    Serial.println(WiFi.localIP());
+  });
+
+  wifi->connect();
+  
+  
+  //WiFi.begin(ssid, password);
+  Serial.println("");                 // Clear the buffer.
+  
+  display.clearDisplay();
+    
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  //display.setTextColor(BLACK, WHITE); // 'inverted' text
+  display.setCursor(0,0);
+  display.println("Connecting");
+  
+
+  
+  while (WiFi.status() != WL_CONNECTED) {    // Wait for connection
+    delay(500);
+    Serial.print(".");
+    display.print(".");
+    display.display();
+    lp++;
+    if(lp>=20) break;
+  }
+  
+  
+  Serial.println("");
+  Serial.print("Connected to ");
+  //Serial.println(ssid);
+  Serial.print("IP address: ");  Serial.println(WiFi.localIP());
+  
+  display.clearDisplay();    
+  display.setTextSize(2);       display.setTextColor(WHITE);  
+  display.setCursor(0,0);       display.println(wifi->SSID());  
+  display.setCursor(0,18);      display.println(WiFi.localIP());
+  //display.setCursor(0,36);      display.println(WiFi.localIP());
+  
+  display.display();
+  delay(2000);
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); 
+  
+}//setup
+
+
+
+//===============================================================
+void loop() {
+ 
+ 
+  unsigned long currentMillis = millis();
+    
+  if(currentMillis - previousMillis >= interval) {     
+      previousMillis = currentMillis;
+      digitalWrite(BUILTIN_LED,LOW);
+    
+      // Reading temperature or humidity takes about 250 milliseconds!
+      // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+      float h = dht.readHumidity();
+      // Read temperature as Celsius
+      float t = dht.readTemperature();
+    
+       
+      // Check if any reads failed and exit early (to try again).
+      if (isnan(h) || isnan(t)) {
+        Serial.println("Failed to read from DHT sensor!");
+        
+        display.clearDisplay();
+        display.setTextSize(2);
+        display.setTextColor(WHITE);
+        display.setCursor(0,22);
+        display.println("Sensor");
+        display.println("Error !!!");
+        display.display();
+
+        h = 0; t = 0;
+        //delay(2000);
+        //return;
+      }
+      
+      // show in serial monitor
+        Serial.print("Humidity: "); 
+        Serial.print(h);
+        Serial.print(" %\t");
+        Serial.print("Temp: "); 
+        Serial.print(t);
+        Serial.print(" *C \n");
+      
+      if (showState == LOW){
+        showState = HIGH; 
+        showHud(t,h);        
+      }
+      else{    
+         showState = LOW; 
+         showTemp(t,h);                      // show temp
+      }
+      
+      
+    
+     lp++;      
+      if(lp >= lp_time){
+        lp=0;
+
+        StaticJsonBuffer<200> jsonBuffer;
+        JsonObject& root = jsonBuffer.createObject();
+        root["temperature"] = t;
+        root["humidity"] = h;
+        
+        // append a new value to /logDHT
+        String name = Firebase.push("/sensor/dht", root);
+        // handle error
+        if (Firebase.failed()) {
+            Serial.print("Firebase Pushing /sensor/dht failed:");
+            Serial.println(Firebase.error()); 
+            display.clearDisplay();  
+            display.setTextSize(2);
+            display.setTextColor(WHITE);
+            display.setCursor(0,0);
+            display.println(" Firebase ");
+             
+            display.setTextSize(3);                               // Size4 = 5 digit , size3 = 7 digits
+            //display.setTextColor(BLACK, WHITE);                 // 'inverted' text
+            display.setTextColor(WHITE);
+            display.setCursor(0,24);
+                   
+            display.println("ERROR");  
+            display.display();
+            
+            digitalWrite(BUILTIN_LED,HIGH); 
+            delay(2000);             
+            lp = 10;                              // repeat to sent again 
+            return;
+        }else{
+            Serial.print("Firebase Pushed /sensor/dht ");
+            Serial.println(name);
+         
+            display.clearDisplay();  
+            display.setTextSize(2);
+            display.setTextColor(WHITE);
+            display.setCursor(0,0);
+            display.println(" Firebase ");
+             
+            display.setTextSize(2);                               // Size4 = 5 digit , size3 = 7 digits
+            //display.setTextColor(BLACK, WHITE);                 // 'inverted' text
+            display.setTextColor(WHITE);
+            display.setCursor(0,24);
+                   
+            display.println("Update OK");  
+            display.display();
+            digitalWrite(BUILTIN_LED,HIGH); 
+            delay(2000); 
+        }
+      }
+      digitalWrite(BUILTIN_LED,HIGH);                   
+  }
+  
+ 
+}//Loop
+
+
+
+
